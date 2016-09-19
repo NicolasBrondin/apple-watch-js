ffc26e32c4018a0664af20f60b88b14aba646bc8
TheCreatorFr <MrBrondinNicolas@gmail.com>
2016-09-19 19:35:03 +0200

    [IMPROVEMENT] physics and displaying improved

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
 var App = function(){
     
-    this.time_interval= 10;
-    this.gravity= 5;
+    this.time_interval= 1;
+    this.gravity= 1;
     this.gravity_center= {x:250, y:250};
     this.canvas;
     this.context;
     this.children= [];
     
     this.update = function() {
-        this.context.clearRect(0,0,500,500);
        for(item of this.children) {
             item.update();
-           //console.log(item);
         }
         
     }.bind(this);
  
     this.init = function() {
-        this.canvas = document.getElementById("universe");
-        this.context = this.canvas.getContext('2d');
-        this.children.push(new CircleObject({index:1, parent: this, x:25, y:25, radius:25}));
-        this.children.push(new CircleObject({index:2,parent: this, x:400, y:400, radius:25}));
-        this.children.push(new CircleObject({index:3,parent: this, x:200, y:400, radius:25}));
-        this.children.push(new CircleObject({index:4,parent: this, x:400, y:40, radius:25}));
-        this.children.push(new CircleObject({index:5,parent: this, x:40, y:400, radius:25}));
+        for(i=0;i<20;i++) {
+            var c = new CircleObject({index:1, parent:this, x: Math.random()*500,y:Math.random()*500,radius: 20});
+            this.children.push(c);
+        }
         setInterval(this.update,this.time_interval);
         
        
     };
 };
-
-      
-      
-      
-      
-
 
 var CircleObject = function(options){
     
@@ -42,33 +31,33 @@ var CircleObject = function(options){
     this.index = options.index;
     this.x = options.x;
     this.y = options.y;
+    this.density = options.density || 1;
+    //http://www.school-for-champions.com/science/gravity_equations_falling_velocity.htm#.V9z8kSuznNU
+    //this.weight = this.radius * density;
     this.radius = options.radius;
-    this.element = "<circle>"+this.index+"</circle>";
+    this.element = document.createElement("circle");
+    document.body.appendChild(this.element); 
+    this.element.style.width = this.radius*2+"px";
+    this.element.style.height = this.radius*2+"px";
     
                     
     this.collide = function(c, newX, newY) {
-        var distance = Math.sqrt(Math.pow(c.x-newX,2)+Math.pow(c.y-newY,2));
-        console.log("circle("+this.index+") distance:"+distance);
-        if(distance<=(c.radius+this.radius))
-            return true;
-        else
-            return false;
+        var collision = {x:false, y:false};
+        collision.x = Math.sqrt(Math.pow(c.x-newX,2)) >= (c.radius+this.radius)*2 ? false : true ;
+        collision.y = Math.sqrt(Math.pow(c.y-newY,2)) >= (c.radius+this.radius)*2 ? false : true ;
+        console.log(collision);
+        return collision;
     };
     
     this.update = function() {
-        console.log("circle("+this.index+") old position:"+this.x,this.y);
+        //console.log("circle("+this.index+") old position:"+this.x,this.y);
         var newX = this.x, newY = this.y;
-        if(this.x<this.parent.gravity_center.x) {
-            newX = this.x + this.parent.gravity;
-        } else if(this.x>this.parent.gravity_center.x) {
-            newX = this.x - this.parent.gravity;
-        }
-        //y
-        if(this.y<this.parent.gravity_center.y) {
-            newY = this.y + this.parent.gravity;
-        } else if(this.y>this.parent.gravity_center.y) {
-            newY = this.y - this.parent.gravity;
-        }
+        
+        var vector = {x: this.parent.gravity_center.x - this.x, y:this.parent.gravity_center.y - this.y};
+        var vector_length = Math.sqrt(Math.pow(200,2) + Math.pow(300,2));
+        var vector_normalized = {x: vector.x/vector_length, y:vector.y/vector_length };
+        newX = this.x+(vector_normalized.x*this.parent.gravity);
+        newY = this.y+(vector_normalized.y*this.parent.gravity);
         
         var collision = false;
         for(c of this.parent.children) {
@@ -79,17 +68,15 @@ var CircleObject = function(options){
             } else {
             }
         }
-        console.log("circle("+this.index+") collision:"+collision);
-        if(!collision) {
+        //console.log("circle("+this.index+") collision:"+collision);
+        if(!collision.x) 
             this.x = newX;
+        if(!collision.y)
             this.y = newY;
-        }
-        console.log("circle("+this.index+") new position:"+this.x,this.y);
-        
-        this.parent.context.beginPath();
-        this.parent.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        this.parent.context.fillStyle = 'green';
-        this.parent.context.fill();
+            
+        //console.log("circle("+this.index+") new position:"+this.x,this.y);
+        this.element.style.left = this.x+"px";
+        this.element.style.top = this.y+"px";
         
     };
 };
